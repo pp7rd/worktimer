88d8d56340bf58cf9451d9f05ae4b7360c16d64a
pp7rd <pptrd@protonmail.com>
2021-06-12 14:27:58 +0200

    Change the card layout

--- lib/view/project-page.dart
+++ lib/view/project-page.dart
@@ -5,14 +5,46 @@ import 'package:worktimer/styles/sizes.dart';
 import 'package:worktimer/viewmodel/projects-vm.dart';
 import '../model/project.dart';
 
+Widget _projectContainer(Project project, BuildContext context, double w, double h) => Container (
+  width: 0.9 * w,
+  height: h,
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        height: 0.5 * h,
+        alignment: Alignment.centerLeft,
+        child: Text(project.name,
+            style:
+                Theme.of(context).textTheme.headline3.merge(
+                      TextStyle(
+                          letterSpacing: 0.1,
+                          fontSize: 0.3 * h),
+                    )),
+      ),
+      Container(
+        height: 0.3 * h,
+        child: Text('1h 23m spent'),
+      )
+    ],
+  ),
+);
+
 Widget _projectCard(BuildContext context, Project project) {
-  var w = 0.85 * width(context);
+  var w = 0.5 * width(context);
   var h = 0.2 * width(context);
 
   return Container(
+      margin: EdgeInsets.fromLTRB(
+        0.005 * w, 0.2 * h, 0.005 * w, 0),
       width: w,
       height: h,
       child: Card(
+          shape: ContinuousRectangleBorder(
+            side: BorderSide(color: Colors.grey.shade600, width: 3),
+            borderRadius: BorderRadius.circular(0)
+          ),
           child: InkWell(
               onTap: () {
                 print('Card tapped.');
@@ -23,39 +55,15 @@ Widget _projectCard(BuildContext context, Project project) {
                     width: 0.1 * w,
                     height: h,
                   ),
-                  Container(
-                    width: 0.9 * w,
-                    height: h,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 0.5 * h,
-                          alignment: Alignment.centerLeft,
-                          child: Text(project.name,
-                              style:
-                                  Theme.of(context).textTheme.headline3.merge(
-                                        TextStyle(
-                                            letterSpacing: 0.1,
-                                            fontSize: 0.3 * h),
-                                      )),
-                        ),
-                        Container(
-                          height: 0.3 * h,
-                          child: Text('1h 23m spent'),
-                        )
-                      ],
-                    ),
-                  )
+                  _projectContainer(project, context, w, h)
                 ],
               ))));
 }
 
 Widget _projectList(BuildContext context, List<Project> projects) => Container(
       //color: Colors.green.shade100,
-      width: 0.95 * width(context),
-      height: 0.85 * height(context),
+      width: 0.85 * width(context),
+      height: 0.75 * height(context),
       child: ListView.builder(
         itemCount: projects.length,
         itemBuilder: (context, index) {
@@ -64,8 +72,45 @@ Widget _projectList(BuildContext context, List<Project> projects) => Container(
       ),
     );
 
+// Play icon
+Widget _addButton(BuildContext context) => IconButton(
+      icon: Icon(Icons.add_rounded),
+      color: Colors.grey.shade800,
+      onPressed: () => {},
+      iconSize: 0.075 * height(context),
+    );
+
+// Pause icon
+Widget _searchButton(BuildContext context) => IconButton(
+      icon: Icon(Icons.search_rounded),
+      color: Colors.grey.shade800,
+      onPressed: () => {},
+      iconSize: 0.075 * height(context),
+    );
+
+// Stop icon
+Widget _deleteButton(BuildContext context) => IconButton(
+      icon: Icon(Icons.delete_outline_rounded),
+      color: Colors.grey.shade800,
+      onPressed: () => {},
+      iconSize: 0.075 * height(context),
+    );
+
+// Buttons
+  Widget _buttons(BuildContext context) => Container(
+        height: 0.15 * height(context),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _addButton(context),
+            _searchButton(context),
+            _deleteButton(context),
+          ],
+        ),
+      );
+
 class ProjectPage extends StatefulWidget {
-  ProjectsVM projectsVM;
+  final ProjectsVM projectsVM;
 
   // Constructor
   ProjectPage(this.projectsVM);
@@ -99,6 +144,7 @@ class _ProjectPage extends State<ProjectPage> {
               height: 0.01 * height(context),
             ),
             _projectList(context, _projects),
+            _buttons(context)
           ],
         ),
       ),
